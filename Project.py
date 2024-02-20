5ee4ea57d17f777b67087b92362cb4a1adcf9251
davidlandy05 <113514395+davidlandy05@users.noreply.github.com>
2024-02-20 10:19:24 +0000

    Add files via upload

--- Project.py
+++ Project.py
@@ -1,17 +1,24 @@
-import serial
 import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import serial
  
 ser = serial.Serial()
 ser.baudrate = 115200
 ser.port = "COM4"
 ser.open()
- 
+
+cred = credentials.Certificate("C:/Users/David Cummins/Downloads/david-6d9f5-firebase-adminsdk-y5nwf-40b213a236.json")
+firebase_admin.initialize_app(cred,{'https://leaving-cert-project-9a808-default-rtdb.europe-west1.firebasedatabase.app/'})
+ref = db.reference()
+
 while True:
     mb_one = str(ser.readline().decode('utf-8'))
     print(mb_one)
     mb_one = mb_one.replace(" ","")
     mb_one = mb_one.replace("\r\n","")
-    print(mb_one)
+    print("You have walked "+mb_one)
     
     
     
