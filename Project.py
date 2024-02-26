1e9c505ea50f87a0e986f61a9e21ea18174b50d8
davidlandy05 <113514395+davidlandy05@users.noreply.github.com>
2024-02-26 15:15:08 +0000

    Add files via upload

--- Project.py
+++ Project.py
@@ -6,19 +6,22 @@ import serial
  
 ser = serial.Serial()
 ser.baudrate = 115200
-ser.port = "COM4"
+ser.port = "COM5"
 ser.open()
 
-cred = credentials.Certificate("C:/Users/David Cummins/Downloads/david-6d9f5-firebase-adminsdk-y5nwf-40b213a236.json")
-firebase_admin.initialize_app(cred,{'https://leaving-cert-project-9a808-default-rtdb.europe-west1.firebasedatabase.app/'})
+cred = credentials.Certificate("C:/Users/18DLandy.ACC/Downloads/leaving-cert-project-9a808-firebase-adminsdk-xb3od-0192c6e1b1.json")
+firebase_admin.initialize_app(cred,{'databaseURL':'https://leaving-cert-project-9a808-default-rtdb.europe-west1.firebasedatabase.app/'})
 ref = db.reference()
+ref.update({'Steps ran':''})
+ref = db.reference().child('Steps ran')
 
 while True:
     mb_one = str(ser.readline().decode('utf-8'))
     print(mb_one)
     mb_one = mb_one.replace(" ","")
     mb_one = mb_one.replace("\r\n","")
-    print("You have walked "+mb_one)
+    print("You have walked ",mb_one," steps")
+    ref.update({str(int(time.time())):{'Steps ran':mb_one}})
     
     
     
